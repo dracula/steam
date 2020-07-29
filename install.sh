805e56d9fcccc57062f24ce914891dcd977d2d80
Tim Schneider <go4it.1@gmx.de>
2020-07-29 18:55:20 +0200

    prettified install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Installing CSS…"
+
+originalfile="../../../steamui/css/libraryroot.css"
+copiedfile="../../../steamui/css/steamlibraryroot.css"
+
+#stores word count of originalfile
+originalfilesize=$(wc -c $originalfile | awk '{print $1}')
+
+if [ $originalfilesize -gt 100 ] ; then
+  #checks if libraryroot.css is from steam and copies it if true
+  echo "renaming libraryroot.css"
+  mv $originalfile $copiedfile
+
+else 
+  #if the original libraryroot.css is not there an error occured
+  if [ ! -e $copiedfile ] ; then
+    echo "The original libraryroot.css file is missing"
+    echo "Pls remove all files in /steamui/css/ and start steam without flags to regain missing files"
+  fi
+fi
+
+cd ../css/
+
+echo "copying custom css files into steamui/css"
+
+for f in *.css
+do
+  cp "$f" ../../../steamui/css/
+done
+
+echo "successful copied custom css files"
--- scripts/moveOriginalCSS.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-
-cd ../../../steamui/css/
-mv libraryroot.css steamlibraryroot.css
