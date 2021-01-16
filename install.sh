901de2e44cb0c7fcc55f7182316523426f786735
Tim Schneider <tim.schneider1337@gmail.com>
2021-01-16 10:10:21 +0100

    made necessary name changes, due to steam renaming its used css from libraryroot.css to 5.css 🤦

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 echo "Installing CSS…"
 
-originalfile="../../steamui/css/libraryroot.css"
-copiedfile="../../steamui/css/steamlibraryroot.css"
+originalfile="../../steamui/css/5.css"
+copiedfile="../../steamui/css/steam5.css"
 
 #stores word count of originalfile
 originalfilesize=$(wc -c $originalfile | awk '{print $1}')
 
 if [ $originalfilesize -gt 100 ] ; then
   #checks if libraryroot.css is from steam and copies it if true
-  echo "renaming libraryroot.css"
+  echo "renaming 5.css"
   mv $originalfile $copiedfile
 
 else 
   #if the original libraryroot.css is not there an error occured
   if [ ! -e $copiedfile ] ; then
-    echo "The original libraryroot.css file is missing"
+    echo "The original 5.css file is missing"
     echo "Pls remove all files in /steamui/css/ and start steam without flags to regain missing files"
   fi
 fi
