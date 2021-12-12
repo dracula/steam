c25b1d1bd91a80074fb3531928e90b27fb5f0e36
koplo199 <85577251+koplo199@users.noreply.github.com>
2021-12-12 01:10:34 +0100

    Add custom loading screen color

--- install.sh
+++ install.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 echo "Installing CSS…"
 
-originalfile="../../steamui/css/5.css"
-copiedfile="../../steamui/css/steam5.css"
+declare -a originalfiles=( "../../steamui/css/6.css" "../../steamui/css/library.css" )
+declare -a copiedfiles=( "../../steamui/css/steam6.css" "../../steamui/css/steamlibrary.css" )
 
-#stores word count of originalfile
-originalfilesize=$(wc -c $originalfile | awk '{print $1}')
+for i in "${!originalfiles[@]}"; do
 
-if [ $originalfilesize -gt 100 ] ; then
-  #checks if libraryroot.css is from steam and copies it if true
-  echo "renaming 5.css"
-  mv $originalfile $copiedfile
+  #stores word count of originalfile
+  originalfilesize=$(wc -c ${originalfiles[i]} | awk '{print $1}')
+  if [ $originalfilesize -gt 100 ] ; then
+    echo "renaming css files"
+    mv ${originalfiles[i]} ${copiedfiles[i]}
 
-else 
-  #if the original libraryroot.css is not there an error occured
-  if [ ! -e $copiedfile ] ; then
-    echo "The original 5.css file is missing"
-    echo "Pls remove all files in /steamui/css/ and start steam without flags to regain missing files"
+  else 
+    #if the original libraryroot.css is not there an error occured
+    if [ ! -e ${copiedfiles[i]} ] ; then
+      echo "The original ${originalfiles[i]} file is missing"
+      echo "Pls remove all files in /steamui/css/ and start steam without flags to regain missing files"
+    fi
   fi
-fi
+
+done
 
 cd css/
 
