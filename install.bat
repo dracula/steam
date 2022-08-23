18ae048b2221e15571744cdaafe44fa486731d2d
Adith <60130987+Sspirax@users.noreply.github.com>
2022-08-23 22:45:38 +0530

    Fix for the renaming of .css files (Closes #36) (#35)
    * Fix for the renaming of .css files
    
    * Fix for the renaming of .css files

--- install.bat
+++ install.bat
@@ -3,8 +3,8 @@ setlocal
 
 echo Installing CSS...
 
-set originalfile=..\..\steamui\css\6.css
-set copiedfile=..\..\steamui\css\steam6.css
+set originalfile=..\..\steamui\css\1010.css
+set copiedfile=..\..\steamui\css\steam1010.css
 set originallibrary=..\..\steamui\css\library.css
 set copiedlibrary=..\..\steamui\css\steamlibrary.css
 
@@ -12,12 +12,12 @@ FOR /F "usebackq" %%A IN ('%originalfile%') DO set "originalfilesize=%%~zA"
 set /A originalfilesize=%originalfilesize% + 0
 
 if %originalfilesize% gtr 100 (
-	echo Renaming 6.css
+	echo Renaming 1010.css
 	move %originalfile% %copiedfile%
 	move %originallibrary% %copiedlibrary%
 ) else (
 	if NOT exist %copiedfile% (
-		echo The original 6.css file is missing
+		echo The original 1010.css file is missing
 		echo Please remove all files in /steamui/css/ and start Steam without flags to regain missing files
 	)
 )
